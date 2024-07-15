e569defa769b4e8e3cb0674a4731a7a0411b1b9f
Jonas Hartmann <jonas.m.hartmann@protonmail.com>
2024-07-15 14:48:53 +0100

    add docstring to times.calculate_fastest_time

--- rse_best_practices_playground/times.py
+++ rse_best_practices_playground/times.py
@@ -12,6 +12,10 @@ my_level = "beginner"  # beginner or pro
 # All students should try first to solve the problem at this level.
 # ===============================================================
 def calculate_fastest_time(time_list):
+    """Loop over list of times and find the fastest.
+    
+    time_list : list of times
+    """
     fastest_time = time_list[0]
     for time in time_list:
         if time > fastest_time:
